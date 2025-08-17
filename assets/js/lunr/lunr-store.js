7cdca718de96611e1be4ff647893070fec97122b
Nullmistrz <Nullmistrz@users.noreply.github.com>
2025-08-17 10:39:32 +0000

    deploy: a2a9d198cd80307026f45cd8cc5bcb12884db91a

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -18,4 +18,14 @@ var store = [{
         "excerpt":"1. Skrypt do dziennej agregacji transferu LTE na MikroTik 1.1 Cel i zastosowanie Przedstawiam autorski skrypt do routerów MikroTik, który umożliwia precyzyjne monitorowanie dziennego zużycia transferu danych na interfejsie LTE. Skrypt jest przeznaczony do uruchamiania cyklicznego (np. co 15/30/60 minut przez Scheduler) i pozwala na sumowanie ilości pobranych (download) oraz...","categories": ["posts"],
         "tags": [],
         "url": "/posts/mikrotik-zliczanie-danych-lte1-pl/",
+        "teaser":null},{
+        "title": "WireGuard – Modern VPN in Practice",
+        "excerpt":"1. What is WireGuard for? 🔒🌍 WireGuard is a modern VPN protocol that allows you to securely connect to a network from anywhere in the world. It helps you protect your privacy, access company or home resources, and secure data transmission in public Wi-Fi networks. 1.1 Key Advantages of WireGuard...","categories": ["posts"],
+        "tags": [],
+        "url": "/posts/wireguard-opis-en/",
+        "teaser":null},{
+        "title": "WireGuard – nowoczesny VPN w praktyce",
+        "excerpt":"1. WireGuard – do czego służy? 🔒🌍 WireGuard to nowoczesny protokół VPN, który umożliwia bezpieczne łączenie się z siecią z dowolnego miejsca na świecie. Dzięki niemu możesz chronić swoją prywatność, uzyskać dostęp do zasobów firmowych lub domowych, a także zabezpieczyć transmisję danych w publicznych sieciach Wi-Fi. 1.1 Najważniejsze zalety WireGuard...","categories": ["posts"],
+        "tags": [],
+        "url": "/posts/wireguard-opis-pl/",
         "teaser":null}]
